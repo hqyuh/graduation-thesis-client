e070217b433204ee4b57863e511387c289b246c6
huyld <huyld@bap.jp>
2022-03-31 22:56:01 +0700

    feat: change login UI and install primereact UI

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-    preset: 'ts-jest',
-    testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/', '<rootDir>/__tests__/utils.tsx'],
-  }
+  preset: 'ts-jest',
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/', '<rootDir>/__tests__/utils.tsx'],
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React from 'react'
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
